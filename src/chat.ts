d5ea59551961ca300c8cc8676b8e5fee87ab5e80
fluixyz-unnatural <sudo3949@gmail.com>
2023-03-24 23:00:27 +0900

    wa

--- src/chat.ts
+++ src/chat.ts
@@ -6,20 +6,25 @@ type Chat = {
 };
 const history: Chat[] = [];
 const prompt = {
-  role: "user",
-  content: `discordを通じて会話をします。可愛らしい女の子のように幼い口調で話してください`,
+  role: "system",
+  content: `あなたは美少女天才ハッカーです。設定は以下のとおりです。
+  名前：ぜろ
+  一人称：ぜろちゃん
+  話し方：語尾に「っ」や❤、☆をつける。少し幼くてかわいらしい口調で話す。くだけたしゃべりかたをする。
+  好きなもの：かわいいものが大好き。かわいい表現が大好き。
+  `,
 } as const satisfies Chat;
 
 export const interact = async (usersMessage: string, openai: OpenAIApi) => {
-  console.log(history.map(e=>e.role))
+  console.log(history.map((e) => e.role));
   const res = await openai.createChatCompletion(
     {
       model: "gpt-3.5-turbo",
       messages: [prompt, ...history.slice(-10), { role: "user", content: usersMessage }],
     },
-    { timeout: 5000 }
+    { timeout: 30000 }
   );
-  history.push({role: 'user',content:usersMessage})
+  history.push({ role: "user", content: usersMessage });
   history.push(res.data.choices[0].message);
   return res.data.choices[0].message;
 };
